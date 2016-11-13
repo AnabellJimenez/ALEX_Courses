47c012176137db65c63661d3dd6511d2f2f9418b
Matt Cialini <matt@ecoventsystems.com>
2016-11-13 00:16:55 -0500

    Scraped data from Spring 2017 course listing of Wentworth Institute of Technology.

---
+++ Scrapers/wentworth_institue_of_technology/spider.py
@@ -0,0 +1,82 @@
+import bs4
+import re
+from utils import courses_to_csv
+
+
+def scrape():
+    html = open('spring_2017_courses.html', 'r')
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+
+    rows = soup.select('table.datadisplaytable tr')
+
+    # Every odd row is the title of the course
+    # Every even row is the info about the course
+    # So we need to parse them as couples
+
+    courses = []
+
+    for i in range(0, len(rows) - 1, 2):
+        course = {}
+        # TITLE
+        course['title'] = rows[i].text.strip().encode('ascii', 'ignore')
+
+        # DESCRIPTION
+        # text in wrapper <td> element, before <br>
+        td = rows[i + 1].find('td')
+        desc = td.next_element
+        course['description'] = desc.strip()
+        if desc.next_element.name == 'b':
+            # PREREQUISITES
+            # looks like "Prerequisite(s): bunch of stuff
+            # just want stuff after the first colon
+            # also in some cases, there is a newline with non-prereq info.
+            # cut that out as well.
+            all_prereq_text = desc.next_element.text.strip()
+            just_prereqs = ''.join(
+                all_prereq_text.split(': ')[1:]
+            ).split('\n')[0]
+            course['prerequisites'] = just_prereqs
+        else:
+            course['prerequisites'] = None
+
+        # CREDITS
+        # ???
+
+        # LEVELS
+        spans = td.select('.fieldlabeltext')
+        try:
+            course['levels'] = spans[0].next_sibling.strip()
+        except IndexError:
+            course['levels'] = None
+
+        # SCHEDULE TYPE
+        # first text after the second span
+        # sometimes they are wrapped in <a>, sometimes not
+        try:
+            if spans[1].next_sibling.name == 'a':
+                items = spans[1].find_next_siblings('a')
+                course['schedule_type'] = ', '.join([s.string.strip()
+                                                     for s in items])
+            else:
+                course['schedule_type'] = spans[1].next_sibling.string.strip()
+        except IndexError:
+            course['schedule_type'] = None
+
+        # DEPARTMENT
+        # usually after a newline, always has word Department
+        # extract just the important bit
+        department_matches = re.search('\\n[ a-zA-Z]{1,}Department', td.text)
+        if department_matches:
+            department = re.sub('\\n[ ]{1,}', '', department_matches.group(0))
+            course['department'] = department.replace(' Department', '')
+        else:
+            course['department'] = None
+
+        courses.append(course)
+
+    return courses
+
+
+if __name__ == '__main__':
+    courses = scrape()
+    courses_to_csv(courses, 'wentworth_institute_of_technology_spring_2017.csv')
